22fb818f9dd176b2f3bab7d99992889c4da7e777
messi-wpy <904315105@qq.com>
2019-03-20 21:22:54 +0800

    加载长图

--- app/src/main/java/com/example/studyandtestapp/CustomView/LargeImageView.java
+++ app/src/main/java/com/example/studyandtestapp/CustomView/LargeImageView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapRegionDecoder;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -19,13 +20,16 @@ public class LargeImageView  extends View {
 
     //该对象可以实现图片的局部显示
     private BitmapRegionDecoder mDecoder;
-    private int ImageWidth,ImageHight;
+    private int width,hight;
     private Rect mRect;
     private static final BitmapFactory.Options option=new BitmapFactory.Options();
     private  boolean isFitXY;
     private float currentImageX,currentImageY;
     private float imageWidth,imageHeight;
     private float lastX,lastY;
+    private float scale;
+    private Matrix matrix=new Matrix();
+    private final static String TAG="TAG";
     /**
      *
      * @param context
@@ -43,10 +47,19 @@ public class LargeImageView  extends View {
     protected void onDraw(Canvas canvas) {
         Log.i("TAG", "onDraw: called");
         if (mDecoder!=null) {
-            Bitmap bmp = mDecoder.decodeRegion(mRect, option);
-            Log.i("TAG", "onDraw: "+bmp.getByteCount());
-            //局部图片占4.7M，缩小了一半内存!
-            canvas.drawBitmap(bmp,0,0,null);
+           if( isFitXY){
+                matrix.setScale(scale,scale);
+
+               Bitmap bmp = mDecoder.decodeRegion(mRect, option);
+               Log.i("TAG", "onDraw: "+bmp.getByteCount());
+               //局部图片占4.7M，缩小了一半内存!
+               canvas.drawBitmap(bmp,matrix,null);
+            }else {
+               Bitmap bmp = mDecoder.decodeRegion(mRect, option);
+               Log.i("TAG", "onDraw: " + bmp.getByteCount());
+               //局部图片占4.7M，缩小了一半内存!
+               canvas.drawBitmap(bmp, 0, 0, null);
+           }
         }
 
     }
@@ -55,9 +68,9 @@ public class LargeImageView  extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         mRect.left=0;
-        mRect.right=getMeasuredWidth();
+        width=mRect.right=getMeasuredWidth();
         mRect.top=0;
-        mRect.bottom=getMeasuredHeight();
+        hight=mRect.bottom=getMeasuredHeight();
         currentImageX=0;
         currentImageY=0;
 
@@ -65,42 +78,63 @@ public class LargeImageView  extends View {
     }
 
 
+    float add = 5;
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
         float moveX=event.getX()-lastX;
         float moveY=lastY-event.getY();
+        if (moveY>0)
+            moveY+=5;
+        else
+            moveY-=5;
+
+
         int action=event.getAction();
         switch (action){
             case MotionEvent.ACTION_DOWN: {
+                Log.i("TAG", "onTouchEvent: -----------DOWN"+event.getY());
                 lastX = event.getX();
                 lastY = event.getY();
                 break;
             }
             case MotionEvent.ACTION_MOVE: {
+                Log.i("TAG", "onTouchEvent: -----------MOVE"+moveY);
                 //水平方向的移动
                 boolean movedX, movedY;
                 movedX = movedY = false;
-
-                // TODO: 19-3-19 优化体验，判断滑动距离来优化 
-                if (mRect.right+moveX <= imageWidth&&mRect.right+moveX>=0&&mRect.left+moveX>=0) {
+                lastX=event.getX();
+                lastY=event.getY();
+                // TODO: 19-3-19 优化体验，判断滑动距离来优化
+              /*  if (moveY>0&&moveY<100)
+                    moveY=50;
+                if ()
+*/
+                if (mRect.right+moveX <= imageWidth &&mRect.left+moveX>=0) {
                     currentImageX += moveX;
                     mRect.left = (int) currentImageX;
                     mRect.right+= (int)moveX;
                     movedX = true;
                 }
 
-                if (mRect.bottom+moveY <= imageHeight&& mRect.bottom+moveY>=0&& mRect.top+moveY>=0) {
+                if (mRect.bottom+moveY < imageHeight&& mRect.top+moveY>0) {
                     currentImageY += moveY;
                     mRect.top= (int) currentImageY;
                     mRect.bottom+=(int)moveY;
                     movedY = true;
+                    Log.i(TAG, "onTouchEvent: -------------------------mrect.bottom"+mRect.bottom);
                 }
+
                 if (movedX || movedY) {
                     //call it to re draw
                     invalidate();
                 }
                 break;
+            }
+            case MotionEvent.ACTION_UP:{
+                add=0;
+                Log.i(TAG, "onTouchEvent: ----------UP");
+
             }
 
         }
@@ -111,15 +145,27 @@ public class LargeImageView  extends View {
 
     public LargeImageView setImage(InputStream image){
         try {
+
             mDecoder=BitmapRegionDecoder.newInstance(image,false);
 
             //这里不能用option来获取图片的宽和高，因为经过BitmapRegionDecoder类处理过后的inputstream不能在获取的其信息，自动返回-1
             imageHeight=mDecoder.getHeight();
             imageWidth=mDecoder.getWidth();
+
+            //先压缩图片
+            int insamplesize=1;
+            while (imageWidth>1.6*width) {
+               imageWidth /= 2;
+               insamplesize*=2;
+            }
+
+            option.inSampleSize=insamplesize;
+            Log.i(TAG, "setImage: ------------"+imageWidth+"   "+imageHeight);
             // TODO: 19-3-19 添加铺满放大缩小选项
             if (isFitXY){
-
-
+                scale=width/imageWidth;
+                imageHeight+=hight-(hight/scale);
+                Log.i(TAG, "setImage: ----------"+imageHeight);
 
             }
 
--- app/src/main/java/com/example/studyandtestapp/MainActivity.java
+++ app/src/main/java/com/example/studyandtestapp/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.studyandtestapp;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.wifi.aware.SubscribeDiscoverySession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.example.studyandtestapp.CustomView.LargeImageView;
 
 import java.io.IOException;
@@ -16,20 +19,27 @@ import java.io.InputStream;
 public class MainActivity extends AppCompatActivity {
 
     private LargeImageView largeImageView;
-    private ImageView imageView;
+    private SubsamplingScaleImageView imageView;
     private Button start_bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        largeImageView=findViewById(R.id.large_image);
-      // imageView=findViewById(R.id.image);
+        //largeImageView=findViewById(R.id.large_image);
+       largeImageView=findViewById(R.id.large_image);
         start_bt=findViewById(R.id.load_1);
+       /* start_bt.setOnClickListener(v -> {
+        imageView.setImage(ImageSource.asset("calander.png"));
+
+        });*/
+
+
         start_bt.setOnClickListener(v->{
             try {
                 String[] files=getAssets().list("");
                 InputStream in=getAssets().open(files[0]);
-                largeImageView.setImage(in);
+                largeImageView.setFitXY(true)
+                                .setImage(in);
                 //Bitmap bmp=BitmapFactory.decodeStream(in);
                 //9.4M 全部加载,也证明了图片占用内存= 分辨率*一个像素点占的内存（ARGB--4,RGB---3）
                 //imageView.setImageBitmap(bmp);
